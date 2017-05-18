dd8650131565b3c2df106f78a1197a9ecfafd870
iamjawa <hello@iamjawa.co>
2017-05-18 12:16:23 +0100

    Started things, organised directories and created static page

--- index.php
+++ index.php
@@ -1,14 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>RunForCoffee</title>
-    <link type="text/css" rel="stylesheet" href="home.css">
+    <link type="text/css" rel="stylesheet" href="css/home.css">
 </head>
+
 <body>
+
     <div class="headerdiv">
         <header class="maintitle">RunForCoffee</header>
+        <img src="images/coffee.png" class="coffeepicture">
     </div>
 
 </body>
+
 </html>
